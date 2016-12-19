64e186b03c389a92c7c6e42c555f699198960e82
Juho Vepsalainen <bebraw@gmail.com>
2016-12-19 09:33:19 +0200

    Port to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackTemplate = require('html-webpack-template');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const pkg = require('./package.json');
 
-const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
@@ -16,37 +15,35 @@ const PATHS = {
   searchIndex: path.join(__dirname, 'app/search_index.json')
 };
 
-process.env.BABEL_ENV = TARGET;
-
 const common = {
   entry: {
     app: PATHS.app,
     style: PATHS.style
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   output: {
     path: PATHS.build,
     filename: '[name].js'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        loaders: ['babel?cacheDirectory'],
+        test: /\.(js|jsx)$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }],
         include: PATHS.app
-      },
-      {
-        test: /\.json?$/,
-        loaders: ['json'],
-        include: PATHS.searchIndex
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'node_modules/html-webpack-template/index.ejs',
+      template: htmlWebpackTemplate,
       title: 'Lunr demo',
       appMountId: 'app',
       inject: false
@@ -54,84 +51,87 @@ const common = {
   ]
 };
 
-if(TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {
-    devtool: 'eval-source-map',
-    devServer: {
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true,
+const dev = merge(common, {
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
 
-      // display only errors to reduce the amount of output
-      stats: 'errors-only',
+    // display only errors to reduce the amount of output
+    stats: 'errors-only',
 
-      // parse host and port from env so this is easy
-      // to customize
-      host: process.env.HOST,
-      port: process.env.PORT
-    },
-    module: {
-      loaders: [
-        // Define development specific CSS setup
-        {
-          test: /\.css$/,
-          loaders: ['style', 'css'],
-          include: PATHS.app
-        }
-      ]
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new NpmInstallPlugin({
-        save: true // --save
-      })
+    // parse host and port from env so this is easy
+    // to customize
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
+  module: {
+    loaders: [
+      // Define development specific CSS setup
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: PATHS.app
+      }
     ]
-  });
-}
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
 
-if(TARGET === 'build' || TARGET === 'stats') {
-  module.exports = merge(common, {
-    entry: {
-      vendor: Object.keys(pkg.dependencies).filter(function(v) {
-        // Exclude alt-utils as it won't work with this setup
-        // due to the way the package has been designed
-        // (no package.json main).
-        return v !== 'alt-utils';
-      })
-    },
-    output: {
-      path: PATHS.build,
-      filename: '[name].[chunkhash].js',
-      chunkFilename: '[chunkhash].js'
-    },
-    module: {
-      loaders: [
-        // Extract CSS during build
-        {
-          test: /\.css$/,
-          loader: ExtractTextPlugin.extract('style', 'css'),
-          include: PATHS.app
-        }
-      ]
-    },
-    plugins: [
-      new CleanPlugin([PATHS.build]),
-      // Output extracted CSS to a file
-      new ExtractTextPlugin('[name].[chunkhash].css'),
-      // Extract vendor and manifest files
-      new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest']
-      }),
-      // Setting DefinePlugin affects React library size!
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
+const build = {
+  entry: {
+    vendor: Object.keys(pkg.dependencies)
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[chunkhash].js'
+  },
+  module: {
+    rules: [
+      // Extract CSS during build
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader'
+        }),
+        include: PATHS.app
+      }
     ]
-  });
-}
+  },
+  plugins: [
+    new CleanPlugin([PATHS.build]),
+    // Output extracted CSS to a file
+    new ExtractTextPlugin('[name].[chunkhash].css'),
+    // Extract vendor and manifest files
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    // Setting DefinePlugin affects React library size!
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
+
+module.exports = function(env) {
+  process.env.BABEL_ENV = env;
+
+  if (env === 'build') {
+    return merge(common, build)
+  }
+
+  return merge(common, dev);
+};
