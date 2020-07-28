c3b16dfccee0a8a31db9f8b181ed57789f4b8a01
Juho Vepsäläinen <bebraw@gmail.com>
2020-07-28 10:07:16 +0200

    chore: Modernize React portion

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# lunr-demo - Demo showing how to use lunr with React and Webpack `require.ensure`
+# lunr-demo - Demo showing how to use lunr with React and webpack `import()`
 
-This little demonstration shows you how to use [lunr](http://lunrjs.com/), [React](https://facebook.github.io/react), and [Webpack](https://webpack.github.io/) to implement static, dynamically loaded search for web. This is useful for small scale usage (think static sites).
+This little demonstration shows you how to use [lunr](http://lunrjs.com/), [React](https://reactjs.org/), and [webpack](https://webpack.js.org/) to implement static, dynamically loaded search for web. This is useful for small scale usage (think static sites).
 
 ## Usage
 
 1. `npm i` - Install dependencies.
-2. `npm run generate_index` - Generate search index.
-3. `npm start` - Start the application.
-4. Open `localhost:8080` in a browser.
+2. `npm start` - Start the application.
+3. Open `localhost:8080` in a browser.
 
 ## License
 
--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,72 +1,63 @@
-import React from 'react';
+import React from "react";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [index, setIndex] = React.useState(null);
+  const [value, setValue] = React.useState("");
+  const [lines, setLines] = React.useState([]);
+  const [results, setResults] = React.useState([]);
 
-    this.state = {
-      index: null,
-      value: '',
-      lines: [],
-      results: [],
-    };
-  }
-  render() {
-    const { results, value } = this.state;
+  const search = (lines, index, query) => {
+    // Search against index and match README lines.
+    return index.search(query.trim()).map((match) => lines[match.ref]);
+  };
 
-    return (
-      <div className="app-container">
-        <div className="search-container">
-          <label>Search against README:</label>
-          <input
-            type="text"
-            value={value}
-            onChange={e => this.onChange(e)} />
-        </div>
-        <div className="results-container">
-          <Results results={results} />
-        </div>
-      </div>
-    );
-  }
-  onChange({ target: { value } }) {
-    const { index, lines } = this.state;
-
+  const onChange = ({ target: { value } }) => {
     // Set captured value to input
-    this.setState(() => ({ value }));
+    setValue(value);
 
     // Search against lines and index if they exist
-    if(lines && index) {
-      return this.setState(() => ({
-        results: this.search(lines, index, value),
-      }));
+    if (lines && index) {
+      setResults(search(lines, index, value));
+
+      return;
     }
 
     // If the index doesn't exist, it has to be set it up.
     // You could show loading indicator here as loading might
     // take a while depending on the size of the index.
-    loadIndex().then(({ index, lines }) => {
-      // Search against the index now.
-      this.setState(() => ({
-        index,
-        lines,
-        results: this.search(lines, index, value),
-      }));
-    }).catch(err => console.error(err));
-  }
-  search(lines, index, query) {
-    // Search against index and match README lines.
-    return index.search(query.trim()).map(
-      match => lines[match.ref],
-    );
-  }
+    loadIndex()
+      .then(({ index, lines }) => {
+        setIndex(index);
+        setLines(lines);
+
+        // Search against the index now
+        setResults(search(lines, index, value));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="app-container">
+      <div className="search-container">
+        <label>Search against README:</label>
+        <input type="text" value={value} onChange={onChange} />
+      </div>
+      <div className="results-container">
+        <Results results={results} />
+      </div>
+    </div>
+  );
 };
 
-const Results = ({results}) => {
-  if(results.length) {
-    return (<ul>{
-      results.map((result, i) => <li key={i}>{result}</li>)
-    }</ul>);
+const Results = ({ results }) => {
+  if (results.length) {
+    return (
+      <ul>
+        {results.map((result, i) => (
+          <li key={i}>{result}</li>
+        ))}
+      </ul>
+    );
   }
 
   return <span>No results</span>;
@@ -78,13 +69,14 @@ function loadIndex() {
   //
   // Note that you will need to shim Promise.all for
   // older browsers and Internet Explorer!
-  return Promise.all([
-    import('lunr'),
-    import('../search_index.json')
-  ]).then(([{ Index }, { index, lines }]) => {
-    return {
-      index: Index.load(index),
-      lines,
-    };
-  });
+  return Promise.all([import("lunr"), import("../search_index.json")]).then(
+    ([{ Index }, { index, lines }]) => {
+      return {
+        index: Index.load(index),
+        lines,
+      };
+    }
+  );
 }
+
+export default App;
